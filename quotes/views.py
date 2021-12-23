ba8d12919933d8a1c725a3244164cd135c49752c
dvvill <91037458+dvvill@users.noreply.github.com>
2021-12-22 22:37:08 -0500

    create quotes views and urls

--- quotes/views.py
+++ quotes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Quote
 
 # Create your views here.
+class QuoteListView(ListView):
+    model = Quote
+    template_name = "home.html"
